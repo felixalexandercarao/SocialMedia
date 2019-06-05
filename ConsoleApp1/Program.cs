655bd8e6c4fdcee28fd8bc5967689b70268f10e7
felixalexandercarao <drlexan211@yahoo.com>
2019-06-05 20:49:44 +0800

    last edit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,17 +17,14 @@ namespace ConsoleApp1
         {
             Access a = new Access();
             PageList PgL = new PageList();
-            Console.WriteLine("Original List");/*I print the original list*/
-            foreach (BasePage page in PgL.GetPageList())
-            {
-                Console.WriteLine("Name: {0} | Public: {1}", page.PageName, page.ViewablePage);
-            }
-            Console.WriteLine("");
 
             a.AccessPageCreate("Public Page"); /*the method for adding a new public page and i print list while still in the method*/
-
+            
             Console.WriteLine("");
+
+            
             Console.WriteLine("I view the list after method is done");/*I print list again*/
+
             foreach (BasePage page in PgL.GetPageList())
             {
                 Console.WriteLine("Name: {0} | Public: {1}", page.PageName, page.ViewablePage);
